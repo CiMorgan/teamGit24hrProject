edec28e9f291990024f0ada290cb3364ded36cbc
Cindy Morgan <camorgan70@gmail.com>
2021-02-04 20:14:19 -0500

    pushing develop branch

--- Room11Note.Models/NoteCreate.cs
+++ Room11Note.Models/NoteCreate.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Room11Note.Models
 {
-    public class NoteCreate
+    public class NoteCreate  //create note
     {
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
